6c4c32bddcc857df4c99db5c53ceac0d57687fcc
timsamuel12 <timsamuel12@gmail.com>
2019-10-30 08:32:41 -0400

    Set up initial commit and php file w HTML body

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang "en">
+<head>
+    <meta charset ="UTF-8">
+    <title>Log In Form</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form method = "post">
+
+    </form>
+</body>
