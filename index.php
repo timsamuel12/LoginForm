7c8b6f264318c2deb530593e4e38c74755ab711f
timsamuel12 <timsamuel12@gmail.com>
2019-10-30 08:47:34 -0400

    Updated w HTML

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
 <body>
     <h1>Login</h1>
     <form method = "post">
-
+        Email:
+        <input type = "text" name = "fname" placeholder = "Enter User Email"><br><br>
+        Password:
+        <input type = "password" name = "pass" placeholder = "Enter User Password"><br><br>
+        <input type = "submit" name = "submit" value = "Submit" >
     </form>
 </body>
